932c5514d24d4a557dcd8aacf7aa146dce264b42
vijay <sanwal@192.168.1.15>
2026-09-21 17:03:11 +0530

    Upgrade EKS add-ons for Kubernetes 1.36

--- addons.tf
+++ addons.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.test.name
-  addon_name   = "vpc-cni"
+  cluster_name  = aws_eks_cluster.test.name
+  addon_name    = "vpc-cni"
+  addon_version = "v1.23.1-eksbuild.1"
 
   depends_on = [
     aws_eks_node_group.nodes
@@ -8,8 +9,9 @@ resource "aws_eks_addon" "vpc_cni" {
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.test.name
-  addon_name   = "coredns"
+  cluster_name  = aws_eks_cluster.test.name
+  addon_name    = "coredns"
+  addon_version = "v1.14.3-eksbuild.23"
 
   depends_on = [
     aws_eks_node_group.nodes
@@ -17,8 +19,9 @@ resource "aws_eks_addon" "coredns" {
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.test.name
-  addon_name   = "kube-proxy"
+  cluster_name  = aws_eks_cluster.test.name
+  addon_name    = "kube-proxy"
+  addon_version = "v1.36.0-eksbuild.25"
 
   depends_on = [
     aws_eks_node_group.nodes
